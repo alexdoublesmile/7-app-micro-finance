ee14eb950a693707c69bb588414afbfd419985e6
Alex Plohoy <alexplohoy@mail.ru>
2020-09-12 20:37:09 +0300

    add to Application: ConsoleView for Banner

--- src/main/java/edu/plohoy/micro/Application.java
+++ src/main/java/edu/plohoy/micro/Application.java
@@ -9,7 +9,7 @@ public class Application {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .sources(Application.class)
-                .bannerMode(Banner.Mode.OFF)
+                .bannerMode(Banner.Mode.CONSOLE)
                 .run(args);
     }
 }
