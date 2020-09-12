977c7529f58c7ff728f84fa57473bcd5b7eda3ff
Alex Plohoy <alexplohoy@mail.ru>
2020-09-12 21:00:08 +0300

    fix rest -> controller

--- src/main/java/edu/plohoy/micro/api/controller/HomeController.java
+++ src/main/java/edu/plohoy/micro/api/controller/HomeController.java
@@ -1,4 +1,4 @@
-package edu.plohoy.micro.api.rest;
+package edu.plohoy.micro.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
