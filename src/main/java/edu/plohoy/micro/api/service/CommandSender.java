a49ba0a8253e11e1c2d61ea6a45cda2cad4d6633
Alex Plohoy <alexplohoy@mail.ru>
2020-09-13 13:00:27 +0300

    add to CommandSender checkAccount method

--- src/main/java/edu/plohoy/micro/api/service/CommandSender.java
+++ src/main/java/edu/plohoy/micro/api/service/CommandSender.java
@@ -2,6 +2,13 @@ package edu.plohoy.micro.api.service;
 
 import org.springframework.stereotype.Controller;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 @Controller
 public class CommandSender {
+
+    public void checkAccount(UUID requsetId, UUID personId, BigDecimal sum) {
+        
+    }
 }
