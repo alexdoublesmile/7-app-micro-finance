346818f970260958d759d36c789b29e89334ee41
Alex Plohoy <alexplohoy@mail.ru>
2020-09-13 12:13:52 +0300

    add to RequestHandler: fields RequestDao & CommandSender

--- src/main/java/edu/plohoy/micro/api/service/RequestHandler.java
+++ src/main/java/edu/plohoy/micro/api/service/RequestHandler.java
@@ -1,7 +1,17 @@
 package edu.plohoy.micro.api.service;
 
+import edu.plohoy.micro.api.dao.RequestDao;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RequestHandler {
+    private RequestDao dao;
+    private CommandSender sender;
+
+    public RequestHandler(RequestDao dao, CommandSender sender) {
+        this.dao = dao;
+        this.sender = sender;
+    }
+
+    
 }
