074be060f862d7f4af44b8146b0d8386623a34a6
Alex Plohoy <alexplohoy@mail.ru>
2020-09-13 12:52:56 +0300

    fill StockServiceImpl

--- src/main/java/edu/plohoy/micro/api/service/StockService.java
+++ src/main/java/edu/plohoy/micro/api/service/StockService.java
@@ -1,4 +1,8 @@
 package edu.plohoy.micro.api.service;
 
+import edu.plohoy.micro.api.domain.StockInfo;
+
 public interface StockService {
+
+    StockInfo getStockInfo(String code);
 }
--- src/main/java/edu/plohoy/micro/impl/service/StockServiceImpl.java
+++ src/main/java/edu/plohoy/micro/impl/service/StockServiceImpl.java
@@ -1,4 +1,20 @@
 package edu.plohoy.micro.impl.service;
 
-public class StockServiceImpl {
+import edu.plohoy.micro.api.domain.StockInfo;
+import edu.plohoy.micro.api.service.StockService;
+import org.springframework.web.client.RestTemplate;
+
+public class StockServiceImpl implements StockService {
+    private final RestTemplate restTemplate;
+    private final String serviceUrl;
+
+    public StockServiceImpl(RestTemplate restTemplate, String serviceUrl) {
+        this.restTemplate = restTemplate;
+        this.serviceUrl = serviceUrl;
+    }
+
+    @Override
+    public StockInfo getStockInfo(String code) {
+        return restTemplate.getForObject(serviceUrl + "api/v1/info" + code, StockInfo.class);
+    }
 }
