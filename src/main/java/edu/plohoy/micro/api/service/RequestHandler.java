79cdfdc938ed27f67fa5d08976556e5daaa8b796
Alex Plohoy <alexplohoy@mail.ru>
2020-09-13 12:58:28 +0300

    add StockService to RequestHandler

--- src/main/java/edu/plohoy/micro/api/service/RequestHandler.java
+++ src/main/java/edu/plohoy/micro/api/service/RequestHandler.java
@@ -2,21 +2,25 @@ package edu.plohoy.micro.api.service;
 
 import edu.plohoy.micro.api.dao.RequestDao;
 import edu.plohoy.micro.api.domain.Request;
+import edu.plohoy.micro.api.domain.StockInfo;
 import edu.plohoy.micro.api.domain.command.CreateRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RequestHandler {
-    private RequestDao dao;
-    private CommandSender sender;
+    private RequestDao requestDao;
+    private CommandSender commandSender;
+    private StockService stockService;
 
-    public RequestHandler(RequestDao dao, CommandSender sender) {
-        this.dao = dao;
-        this.sender = sender;
+    public RequestHandler(RequestDao requestDao, CommandSender commandSender, StockService stockService) {
+        this.requestDao = requestDao;
+        this.commandSender = commandSender;
+        this.stockService = stockService;
     }
 
 
     public void createRequest(CreateRequest command) {
-        dao.insert(Request.from(command));
+        requestDao.insert(Request.from(command));
+        StockInfo stock = stockService.getStockInfo(command.getStockCode());
     }
 }
