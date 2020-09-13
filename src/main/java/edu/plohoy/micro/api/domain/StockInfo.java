8271b14c150926d512d1369fee138b7e05f41216
Alex Plohoy <alexplohoy@mail.ru>
2020-09-13 12:55:53 +0300

    fill StockInfo

--- src/main/java/edu/plohoy/micro/api/domain/StockInfo.java
+++ src/main/java/edu/plohoy/micro/api/domain/StockInfo.java
@@ -1,4 +1,29 @@
 package edu.plohoy.micro.api.domain;
 
+import java.beans.ConstructorProperties;
+import java.math.BigDecimal;
+
 public class StockInfo {
+    private final String stockCode;
+    private final int stockCount;
+    private final BigDecimal price;
+
+    @ConstructorProperties({"stock_code", "stock_count", "price"})
+    public StockInfo(String stockCode, int stockCount, BigDecimal price) {
+        this.stockCode = stockCode;
+        this.stockCount = stockCount;
+        this.price = price;
+    }
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public int getStockCount() {
+        return stockCount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
 }
